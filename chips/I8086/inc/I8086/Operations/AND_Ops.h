83b448adf5df9c327355a333aa0795af2501812a
Dmitriy Filippov <ghettorunner@yandex.ru>
2024-12-09 17:05:07 +0300

    [I8086] AND/XOR ops refactored as OR ops implementation

--- chips/I8086/inc/I8086/Operations/AND_Ops.h
+++ chips/I8086/inc/I8086/Operations/AND_Ops.h
@@ -4,66 +4,71 @@
 #include "Adressing.h"
 
 template<typename T>
-T GenericAND(const T value1, const T value2) {
+T PerformAND(const T value1, const T value2) {
     return value1 & value2;
 }
 
 template<typename T>
-FORCE_INLINE void GenericUpdateStatusAfterAND(I8086 &cpu, const T &value){
+void UpdateStatusAfterAND(I8086 &cpu, const T &value){
     cpu.Status.C = 0;
     cpu.Status.O = 0;
     cpu.Status.UpdateStatusByValue(value, I8086_Status_S | I8086_Status_Z | I8086_Status_P);
 }
 
 template<typename T>
-void UpdateStatusAfterAND(I8086 &cpu, const InstructionResult<T> &instructionResult) {
-    GenericUpdateStatusAfterAND(cpu, instructionResult.leftOp.after);
+void UpdateStatusAfterAND_Wrapper(I8086 &cpu, const InstructionResult<T> &instructionResult) {
+    UpdateStatusAfterAND(cpu, instructionResult.leftOp.after);
+}
+
+template<typename T>
+void I8086_EGx_EGx_AND(Memory &memory, I8086 &cpu, InstructionDirection direction) {
+    I8086_EGx_EGx<BYTE>(memory, cpu, &PerformAND, &UpdateStatusAfterAND_Wrapper, direction);
 }
 
 //  Mem8 <-- Mem8 AND Reg8
 void I8086_AND_Eb_Gb(BYTE OpCode, Memory &memory, I8086 &cpu) {
-    I8086_EGx_EGx<BYTE>(memory, cpu, &GenericAND, &UpdateStatusAfterAND, InstructionDirection::MemReg_Reg);
+    I8086_EGx_EGx_AND<BYTE>(memory, cpu, InstructionDirection::MemReg_Reg);
 }
 
 //  Mem16 <-- Mem16 AND Reg16
 void I8086_AND_Ev_Gv(BYTE OpCode, Memory &memory, I8086 &cpu) {
-    I8086_EGx_EGx<WORD>(memory, cpu, &GenericAND, &UpdateStatusAfterAND, InstructionDirection::MemReg_Reg);
+    I8086_EGx_EGx_AND<WORD>(memory, cpu, InstructionDirection::MemReg_Reg);
 }
 
 //  Reg8 <-- Reg8 AND Reg8
 //  Reg8 <-- Reg8 AND Mem8
 void I8086_AND_Gb_Eb(BYTE OpCode, Memory &memory, I8086 &cpu) {
-    I8086_EGx_EGx<BYTE>(memory, cpu, &GenericAND, &UpdateStatusAfterAND, InstructionDirection::Reg_MemReg);
+    I8086_EGx_EGx_AND<BYTE>(memory, cpu, InstructionDirection::Reg_MemReg);
 }
 
 //  Reg16 <-- Reg16 AND Reg16
 //  Reg16 <-- Reg16 AND Mem16
 void I8086_AND_Gv_Ev(BYTE OpCode, Memory &memory, I8086 &cpu) {
-    I8086_EGx_EGx<WORD>(memory, cpu, &GenericAND, &UpdateStatusAfterAND, InstructionDirection::Reg_MemReg);
+    I8086_EGx_EGx_AND<WORD>(memory, cpu, InstructionDirection::Reg_MemReg);
 }
 
 //  AL <-- AL AND Immediate8
 void I8086_AND_AL_Ib(BYTE OpCode, Memory &memory, I8086 &cpu) {
     BYTE value = cpu.Fetch<BYTE>(memory);
-    cpu.AL = GenericAND(cpu.AL, value);
-    GenericUpdateStatusAfterAND(cpu, cpu.AL);
+    cpu.AL = PerformAND(cpu.AL, value);
+    UpdateStatusAfterAND(cpu, cpu.AL);
 }
 
 //  AX <-- AX AND Immediate16
 void I8086_AND_AX_Iv(BYTE OpCode, Memory &memory, I8086 &cpu) {
     WORD value = cpu.Fetch<WORD>(memory);
-    cpu.AX = GenericAND(cpu.AX, value);
-    GenericUpdateStatusAfterAND(cpu, cpu.AX);
+    cpu.AX = PerformAND(cpu.AX, value);
+    UpdateStatusAfterAND(cpu, cpu.AX);
 }
 
 //  Reg8 <-- Reg8 AND Immediate8
 //  Mem8 <-- Mem8 AND Immediate8
 void I8086_AND_Eb_Ib(Memory &memory, I8086 &cpu, const ModRegByte modRegByte) {
-    I8086_Ex_Ix<BYTE>(memory, cpu, modRegByte, &GenericAND, &UpdateStatusAfterAND);
+    I8086_Ex_Ix<BYTE>(memory, cpu, modRegByte, &PerformAND, &UpdateStatusAfterAND_Wrapper);
 }
 
 //  Reg16 <-- Reg16 AND Immediate16
 //  Mem16 <-- Mem16 AND Immediate16
 void I8086_AND_Ev_Iv(Memory &memory, I8086 &cpu, const ModRegByte modRegByte) {
-    I8086_Ex_Ix<WORD>(memory, cpu, modRegByte, &GenericAND, &UpdateStatusAfterAND);
+    I8086_Ex_Ix<WORD>(memory, cpu, modRegByte, &PerformAND, &UpdateStatusAfterAND_Wrapper);
 }
--- chips/I8086/inc/I8086/Operations/XOR_Ops.h
+++ chips/I8086/inc/I8086/Operations/XOR_Ops.h
@@ -4,66 +4,71 @@
 #include "Adressing.h"
 
 template<typename T>
-T GenericXOR(const T value1, const T value2) {
+T PerformXOR(const T value1, const T value2) {
     return value1 ^ value2;
 }
 
 template<typename T>
-FORCE_INLINE void GenericUpdateStatusAfterXOR(I8086 &cpu, const T &value){
+void UpdateStatusAfterXOR(I8086 &cpu, const T &value){
     cpu.Status.C = 0;
     cpu.Status.O = 0;
     cpu.Status.UpdateStatusByValue(value, I8086_Status_S | I8086_Status_Z | I8086_Status_P);
 }
 
 template<typename T>
-void UpdateStatusAfterXOR(I8086 &cpu, const InstructionResult<T> &instructionResult) {
-    GenericUpdateStatusAfterXOR(cpu, instructionResult.leftOp.after);
+void UpdateStatusAfterXOR_Wrapper(I8086 &cpu, const InstructionResult<T> &instructionResult) {
+    UpdateStatusAfterXOR(cpu, instructionResult.leftOp.after);
+}
+
+template<typename T>
+void I8086_EGx_EGx_XOR(Memory &memory, I8086 &cpu, InstructionDirection direction) {
+    I8086_EGx_EGx<BYTE>(memory, cpu, &PerformXOR, &UpdateStatusAfterXOR_Wrapper, direction);
 }
 
 //  Mem8 <-- Mem8 XOR Reg8
 void I8086_XOR_Eb_Gb(BYTE OpCode, Memory &memory, I8086 &cpu) {
-    I8086_EGx_EGx<BYTE>(memory, cpu, &GenericXOR, &UpdateStatusAfterXOR, InstructionDirection::MemReg_Reg);
+    I8086_EGx_EGx_XOR<BYTE>(memory, cpu, InstructionDirection::MemReg_Reg);
 }
 
 //  Mem16 <-- Mem16 XOR Reg16
 void I8086_XOR_Ev_Gv(BYTE OpCode, Memory &memory, I8086 &cpu) {
-    I8086_EGx_EGx<WORD>(memory, cpu, &GenericXOR, &UpdateStatusAfterXOR, InstructionDirection::MemReg_Reg);
+    I8086_EGx_EGx_XOR<WORD>(memory, cpu, InstructionDirection::MemReg_Reg);
 }
 
 //  Reg8 <-- Reg8 XOR Reg8
 //  Reg8 <-- Reg8 XOR Mem8
 void I8086_XOR_Gb_Eb(BYTE OpCode, Memory &memory, I8086 &cpu) {
-    I8086_EGx_EGx<BYTE>(memory, cpu, &GenericXOR, &UpdateStatusAfterXOR, InstructionDirection::Reg_MemReg);
+    I8086_EGx_EGx_XOR<BYTE>(memory, cpu, InstructionDirection::Reg_MemReg);
 }
 
 //  Reg16 <-- Reg16 XOR Reg16
 //  Reg16 <-- Reg16 XOR Mem16
 void I8086_XOR_Gv_Ev(BYTE OpCode, Memory &memory, I8086 &cpu) {
-    I8086_EGx_EGx<WORD>(memory, cpu, &GenericXOR, &UpdateStatusAfterXOR, InstructionDirection::Reg_MemReg);
+    I8086_EGx_EGx_XOR<WORD>(memory, cpu, InstructionDirection::Reg_MemReg);
 }
 
 //  AL <-- AL XOR Immediate8
 void I8086_XOR_AL_Ib(BYTE OpCode, Memory &memory, I8086 &cpu) {
     const BYTE value = cpu.Fetch<BYTE>(memory);
-    cpu.AL = GenericXOR(cpu.AL, value);
-    GenericUpdateStatusAfterXOR(cpu, cpu.AL);
+    cpu.AL = PerformXOR(cpu.AL, value);
+    UpdateStatusAfterXOR(cpu, cpu.AL);
 }
 
 //  AX <-- AX XOR Immediate16
 void I8086_XOR_AX_Iv(BYTE OpCode, Memory &memory, I8086 &cpu) {
     const WORD value = cpu.Fetch<WORD>(memory);
-    cpu.AX = GenericXOR(cpu.AX, value);
-    GenericUpdateStatusAfterXOR(cpu, cpu.AX);
+    cpu.AX = PerformXOR(cpu.AX, value);
+    UpdateStatusAfterXOR(cpu, cpu.AX);
 }
 
 //  Reg8 <-- Reg8 XOR Immediate8
 //  Mem8 <-- Mem8 XOR Immediate8
 void I8086_XOR_Eb_Ib(Memory &memory, I8086 &cpu, const ModRegByte modRegByte) {
-    I8086_Ex_Ix<BYTE>(memory, cpu, modRegByte, &GenericXOR, &UpdateStatusAfterXOR);
+    I8086_Ex_Ix<BYTE>(memory, cpu, modRegByte, &PerformXOR, &UpdateStatusAfterXOR_Wrapper);
 }
 
 //  Reg16 <-- Reg16 XOR Immediate16
 //  Mem16 <-- Mem16 XOR Immediate16
 void I8086_XOR_Ev_Iv(Memory &memory, I8086 &cpu, const ModRegByte modRegByte) {
-    I8086_Ex_Ix<WORD>(memory, cpu, modRegByte, &GenericXOR, &UpdateStatusAfterXOR);
+    I8086_Ex_Ix<WORD>(memory, cpu, modRegByte, &PerformXOR, &UpdateStatusAfterXOR_Wrapper);
 }
