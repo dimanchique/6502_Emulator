34f1d63f8a1299f4d9fabac05eb23595e9eb60cb
Dmitriy Filippov <ghettorunner@yandex.ru>
2025-02-10 20:26:17 +0300

    [I8086] PUSH/POP namespace removed #DVLP-45

--- chips/I8086/inc/I8086/Operations/PUSH_POP_Ops.h
+++ chips/I8086/inc/I8086/Operations/PUSH_POP_Ops.h
@@ -2,21 +2,18 @@
 
 #include "I8086/I8086.h"
 
-namespace PUSH_POP {
+FORCE_INLINE void PUSH_Reg(I8086& cpu, Memory &memory, const BYTE regIdx) {
+    assert(regIdx <= 7);
+    cpu.PushDataToStack(memory, *cpu.GetRegWordPtr(regIdx));
+}
 
-    FORCE_INLINE void PUSH_Reg(I8086& cpu, Memory &memory, const BYTE regIdx) {
-        assert(regIdx <= 7);
-        cpu.PushDataToStack(memory, *cpu.GetRegWordPtr(regIdx));
-    }
-
-    FORCE_INLINE void POP_Reg(I8086& cpu, Memory &memory, const BYTE regIdx) {
-        assert(regIdx <= 7);
-        *cpu.GetRegWordPtr(regIdx) = cpu.PopDataFromStack(memory);
-    }
+FORCE_INLINE void POP_Reg(I8086& cpu, Memory &memory, const BYTE regIdx) {
+    assert(regIdx <= 7);
+    *cpu.GetRegWordPtr(regIdx) = cpu.PopDataFromStack(memory);
 }
 
 void I8086_PUSH_Reg(BYTE OpCode, Memory &memory, I8086 &cpu) {
-    PUSH_POP::PUSH_Reg(cpu, memory, OpCode - PUSH_AX);
+    PUSH_Reg(cpu, memory, OpCode - PUSH_AX);
 }
 
 void I8086_PUSH_AX(BYTE OpCode, Memory &memory, I8086 &cpu) {
@@ -52,7 +49,7 @@ void I8086_PUSH_DI(BYTE OpCode, Memory &memory, I8086 &cpu) {
 }
 
 void I8086_POP_Reg(BYTE OpCode, Memory &memory, I8086 &cpu) {
-    PUSH_POP::POP_Reg(cpu, memory, OpCode - POP_AX);
+    POP_Reg(cpu, memory, OpCode - POP_AX);
 }
 
 void I8086_POP_AX(BYTE OpCode, Memory &memory, I8086 &cpu) {
