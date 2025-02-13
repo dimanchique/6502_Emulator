7bc1bc04b99e2a74cd21d328c9524169008b0326
Dmitriy Filippov <ghettorunner@yandex.ru>
2025-02-13 20:32:51 +0300

    [I8086] XCHG tests #DVLP-144

--- tests/I8086/src/XCHG_Tests.cpp
+++ tests/I8086/src/XCHG_Tests.cpp
@@ -1,3 +1,4 @@
+#include "I8086_DoubleOpTests.h"
 #include "I8086_ImpliedOpTests.h"
 
 class I8086_XCHG_AX_Fixture : public I8086_ImpliedOpTests {
@@ -39,4 +40,87 @@ TEST_F(I8086_XCHG_AX_Fixture, XCHG_SI_AX) {
 
 TEST_F(I8086_XCHG_AX_Fixture, XCHG_DI_AX) {
     TestXCHG_AX(XCHG_DI_AX, &cpu.DI);
+}
+
+class I8086_XCHG_Fixture : public I8086_DoubleOpFixture {};
+
+TEST_F(I8086_XCHG_Fixture, XCHG_Gv_Ev_AX_BX) {
+    constexpr WORD value1 = 0x0060;
+    constexpr WORD value2 = 0x009A;
+    cpu.AX = value1;
+    cpu.BX = value2;
+    const BYTE leftReg = wAX;
+    const BYTE rightReg = wBX;
+
+    TestRegRegInstruction<WORD>(&leftReg, &rightReg, XCHG_Gv_Ev, 16);
+    EXPECT_EQ(cpu.AX, value2);
+    EXPECT_EQ(cpu.BX, value1);
+}
+
+TEST_F(I8086_XCHG_Fixture, XCHG_Gb_Eb_BL_BX) {
+    constexpr WORD value1 = 0x0060;
+    constexpr WORD value2 = 0x009A;
+    cpu.BL = value1;
+    cpu.DL = value2;
+    const BYTE leftReg = bBL;
+    const BYTE rightReg = bDL;
+
+    TestRegRegInstruction<BYTE>(&leftReg, &rightReg, XCHG_Gb_Eb, 16);
+    EXPECT_EQ(cpu.BL, value2);
+    EXPECT_EQ(cpu.DL, value1);
+}
+
+TEST_F(I8086_XCHG_Fixture, XCHG_Gv_Ev_BX_Addressed_AX) {
+    ModRegByteConstructor modReg;
+
+    modReg.leftOp.archetype = OperandArchetype::Mem;
+    modReg.leftOp.memData.dispSize = 0;
+    modReg.leftOp.memData.mode = modeBX;
+
+    modReg.rightOp.archetype = OperandArchetype::Reg;
+    modReg.rightOp.regData.byteReg = bDH;
+
+    const BYTE refValue = 0xFA;
+    const BYTE memValue = 0xBE;
+
+    cpu.DH = refValue;
+    cpu.BX = 0x009A;
+    cpu.DS = 0x4000;
+    cyclesExpected = 16 + 5;
+
+    const DWORD memAddress = cpu.BX + (cpu.DS << 4);
+    TestMemRegInstruction(memAddress, memValue, XCHG_Gb_Eb, modReg, 16);
+
+    WORD result = mem[memAddress];
+    EXPECT_EQ(result, refValue);
+    EXPECT_EQ(cpu.DH, memValue);
+}
+
+TEST_F(I8086_XCHG_Fixture, OR_Gv_Ev_BXSI_Addressed_WithDisp_AX) {
+    ModRegByteConstructor modReg;
+
+    modReg.leftOp.archetype = OperandArchetype::Mem;
+    modReg.leftOp.memData.dispSize = 1;
+    modReg.leftOp.memData.dispValue = 0xAD;
+    modReg.leftOp.memData.mode = modeBXpSI;
+
+    modReg.rightOp.archetype = OperandArchetype::Reg;
+    modReg.rightOp.regData.wordReg = wAX;
+
+    const WORD refValue = 0xFACE;
+    const WORD memValue = 0xBABE;
+
+    cpu.AX = refValue;
+    cpu.BX = 0x8000;
+    cpu.SI = 0x1000;
+    cpu.DS = 0x2000;
+    cyclesExpected = 16 + 11;
+
+    const DWORD memAddress = cpu.BX + cpu.SI + modReg.leftOp.memData.dispValue + (cpu.DS << 4);
+    TestMemRegInstruction(memAddress, memValue, XCHG_Gv_Ev, modReg, 16);
+
+    WORD result = mem[memAddress];
+    result |= (mem[memAddress + 1] << 8);
+    EXPECT_EQ(result, refValue);
+    EXPECT_EQ(cpu.AX, memValue);
 }
